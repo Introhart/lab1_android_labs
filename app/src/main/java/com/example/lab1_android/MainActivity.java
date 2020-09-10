b822d8c9feb076616404b517ca98d1eb4d2a5501
introhart <Introhart@gmail.com>
2020-09-10 20:03:23 +0700

    lab 2 : completed

--- app/src/main/java/com/example/lab1_android/MainActivity.java
+++ app/src/main/java/com/example/lab1_android/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.lab1_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,42 +21,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText input_num1 = findViewById(R.id.input_num1);
-        final EditText input_num2 = findViewById(R.id.input_num2);
-        final TextView tv_result = findViewById(R.id.textView_result);
-        final RadioButton radio_addition = findViewById(R.id.radio_addition);
-        final RadioButton radio_multiplying = findViewById(R.id.radio_multiplying);
-        Button button_submit = findViewById(R.id.button_calculate);
+        Button button;
 
-        button_submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String s_op1 = input_num1.getText().toString();
-                String s_op2 = input_num2.getText().toString();
-                float op1, op2;
-
-                if(!(s_op1.matches("")) && !(s_op1.matches("")) &&
-                        (radio_addition.isChecked() || radio_multiplying.isChecked())) {
-                    op1 = Float.parseFloat(s_op1);
-                    op2 = Float.parseFloat(s_op2);
-
-                    if(radio_addition.isChecked()){
-                        tv_result.setText(String.valueOf(op1+op2));
-                        Toast.makeText(getApplicationContext(),String.valueOf(op1+op2), Toast.LENGTH_SHORT).show();
-                    }
-                    else{
-                        tv_result.setText(String.valueOf(op1*op2));
-                        Toast.makeText(getApplicationContext(),String.valueOf(op1*op2), Toast.LENGTH_SHORT).show();
-                    }
-
-
+        int screenSizeMask = (getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK);
+        if( screenSizeMask == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+                screenSizeMask == Configuration.SCREENLAYOUT_SIZE_NORMAL
+        ){
+            button = (Button)findViewById(R.id.button_goForward);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getBaseContext(), SecondActivity.class);
+                    startActivity(intent);
                 }
-                else{
-                    Toast.makeText(getApplicationContext(),"Fill in all the fields", Toast.LENGTH_SHORT).show();
-                }
+            });
 
-            }
-        });
+        }
+
+
+
 
     }
 }
